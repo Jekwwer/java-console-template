3dcbbfd0d6b40970f678af0adf0eed76af6490eb
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-10-15 14:10:13 +0200

    Create devcontainer.json

---
+++ src/test/java/com/yourcompany/project/MainTest.java
@@ -0,0 +1,14 @@
+package com.yourcompany.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+  @Test
+  public void testMain() {
+    // Simple test to check if the main method works
+    assertTrue(true); // We don't have complex logic to test yet
+  }
+}
